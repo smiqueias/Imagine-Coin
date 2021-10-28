577a1ebc0312e5c68cfa615274c79d0d8dffb1da
saulo <saulomiqueias7@gmail.com>
2021-10-28 14:41:07 -0300

    setting app images and app color palette

---
+++ lib/src/app/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color orange = Color(0xFFFF9000);
+  static const Color orange2 = Color(0xFFE85D1F);
+  static const Color white = Color(0xFFF4EDE8);
+  static const Color grey1 = Color(0xFF232129);
+  static const Color grey2 = Color(0xFF312E38);
+  static const Color grey3 = Color(0xFF999591);
+  static const Color green = Color(0xFF04D361);
+}
---
+++ lib/src/app/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String icon = "assets/app/icon.png";
+  static const String iconTransparent = "assets/app/icon-transparent.png";
+}
