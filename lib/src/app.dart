60d3239590c80e3109e9be3c7b60519a55fbe794
smiqueias <saulomiqueias7@gmail.com>
2021-11-09 22:15:19 -0300

    app routers changed

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,12 @@
 import 'package:cryptocurrency_app/src/core/app_theme.dart';
 import 'package:cryptocurrency_app/src/core/providers.dart';
+import 'package:cryptocurrency_app/src/features/home/router/home_router.dart';
 import "package:flutter/material.dart";
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-import 'core/app_routers.dart';
+import 'features/coin-detail/router/coin_detail_router.dart';
+import 'features/coin-list/router/coin_list_router.dart';
+import 'features/favorites/router/favorites_router.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -18,8 +21,13 @@ class App extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: "Imagine Coin",
           theme: AppTheme.appTheme,
-          initialRoute: AppRouters.initialRoute,
-          onGenerateRoute: AppRouters.generateRoutes,
+          initialRoute: "/",
+          routes: {
+            "/": (_) => const HomeRouter(),
+            "/coin-list": (_) => const CoinListRouter(),
+            "/coin-detail": (_) => const CoinDetailRouter(),
+            "/favorites": (_) => const FavoritesRouter(),
+          },
         ),
       ),
     );
