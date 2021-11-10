915d029d90692bb11940d436c9845a15694263e0
smiqueias <saulomiqueias7@gmail.com>
2021-11-09 22:14:53 -0300

    add favorites feature

---
+++ lib/src/features/favorites/router/favorites_router.dart
@@ -0,0 +1,11 @@
+import 'package:cryptocurrency_app/src/features/favorites/view/container/favorites_container.dart';
+import 'package:flutter/material.dart';
+
+class FavoritesRouter extends StatelessWidget {
+  const FavoritesRouter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const FavoritesContainer();
+  }
+}
